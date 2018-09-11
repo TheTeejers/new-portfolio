6d1f22fda3990be8db4a5c1a0ce47bfbd9b0d4e6
TheTeejers <30870766+TheTeejers@users.noreply.github.com>
2018-09-11 15:17:33 -0500

    changed back

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+// ReactDOM.render((
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// ), document.getElementById('root'));
